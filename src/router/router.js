f292bf47398fe0002425ca38282696fe25ea11f7
Manuel Navarro <manuelnhc@gmail.com>
2022-08-22 19:52:43 +0100

    ADD Views Calendar, ConstructorDetails, DriverDetails, RaceDetails && UPDATE router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,23 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DriverStandings from "../views/DriverStandings.vue";
+import DriverDetails from "../views/DriverDetails.vue";
 import ConstructorStandings from "../views/ConstructorStandings.vue";
+import ConstructorDetails from "../views/ConstructorDetails.vue";
 import LastResults from "../views/LastRaceResult.vue";
+import Calendar from "../views/Calendar.vue";
+import RaceDetails from "../views/RaceDetails.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Clasificación-Pilotos",
+    name: "Driver-Standings",
     component: DriverStandings,
   },
   {
+    path: "/:id",
+    name: "Driver-Details",
+    component: DriverDetails,
+  },
+  {
     path: "/constructor-standings",
-    name: "Clasificación-Constructores",
+    name: "Constructor-Standings",
     component: ConstructorStandings,
   },
   {
+    path: "/constructor-standings/:id",
+    name: "Constructor-Details",
+    component: ConstructorDetails,
+  },
+  {
     path: "/last-results",
-    name: "Resultados-Última-Carrera",
+    name: "Last-Results",
     component: LastResults,
+  },
+  {
+    path: "/calendar",
+    name: "Calendar",
+    component: Calendar,
+  },
+  {
+    path: "/calendar/:id",
+    name: "Race-Details",
+    component: RaceDetails,
   },
 ];
 
